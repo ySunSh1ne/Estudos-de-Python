188c558a2b42e539d4aa36863fb034950f4ed9a0
Alex Almeida <113144625+ySunSh1ne@users.noreply.github.com>
2022-09-16 01:19:44 -0300

    Dolar$
    Programa que lê quanto dinheiro tem na Conta e lê o valor do dólares e calcula quantos dólares pode comprar

---
+++ exercícios 6.py
@@ -0,0 +1,6 @@
+print('Simulação de compra de dolar')
+print(' ')
+M = float(input("Digite o Valor que você possui na sua conta corrente: "))
+C = float(input('Digite a Cotação atual do Dolar: '))
+
+print('Com {} voce pode comprar {} em Dolar Hoje com a Cotação de {}'.format(M, (M//C), C))
