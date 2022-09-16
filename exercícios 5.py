9b86d1aa26f601d88491734855de684421400542
Alex Almeida <113144625+ySunSh1ne@users.noreply.github.com>
2022-09-16 01:04:03 -0300

    Tabuada
    Um programa que lê um numero qualquer e mostra a sua tabuada

---
+++ exercícios 5.py
@@ -0,0 +1,4 @@
+N = int(input('Digite um Numero para ver a Tabuada dele: '))
+
+print('A Tabuada de {} é \n {} x 1 = {}\n {} x 2 = {}\n {} x 3 = {}\n {} x 4 = {}\n {} x 5 = {}\n'
+      ' {} x 6 = {}\n {} x 7 = {}\n {} x 8 = {}\n {} x 9 = {}\n {} x 10= {}'.format(N,N,(N*1),N,(N*2),N,(N*3),N,(N*4),N,(N*5),N,(N*6),N,(N*7),N,(N*8),N,(N*9),N,(N*10)))
