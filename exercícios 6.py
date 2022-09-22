b79325bb04b9bd6acb7ed97e68f73ccf2e8f0d68
Alex Almeida <113144625+ySunSh1ne@users.noreply.github.com>
2022-09-22 02:24:19 -0300

    Exercício 6
    Calculando Compra de Dolar :3

--- exercícios 6.py
+++ exercícios 6.py
@@ -4,3 +4,4 @@ M = float(input("Digite o Valor que você possui na sua conta corrente: "))
 C = float(input('Digite a Cotação atual do Dolar: '))
 
 print('Com {} voce pode comprar {} em Dolar Hoje com a Cotação de {}'.format(M, (M//C), C))
+
